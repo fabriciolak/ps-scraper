8907141cb261598aa5bc1822fe16dd25ff286061
fabricio <fabriciolak12@gmail.com>
2021-12-19 10:07:53 -0300

    PS-SCRAPER hasbeen 50% completed

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-const puppeteer = require('puppeteer');
-const express = require('express');
+const puppeteer = require("puppeteer");
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Pronto');
-})
+app.get("/", async (req, res) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0);
+  await page.goto(
+    "https://store.playstation.com/pt-br/category/35027334-375e-423b-b500-0d4d85eff784/1"
+  );
+
+  const data = await page.evaluate(() => {
+    const nodeList = document.querySelectorAll(".psw-product-tile");
+
+    const offers = [...nodeList];
+
+    const listOffers = offers.map(item => {
+      return {
+        telemetryMeta: JSON.parse(item.parentElement.dataset.telemetryMeta),
+        thumb: item.querySelector('img').getAttribute('src').replace("?w=54&thumb=true", ""),
+        productType: item.children[1].children[0].innerText,
+        discountBadge: item.querySelector('.psw-discount-badge__text').innerText,
+        discountText: item.querySelector('.sr-only').outerText,
+        platformTag: item.querySelector('.psw-l-cluster').outerText,
+        pageLink: item.parentNode.href
+      }
+    });
+
+    return listOffers;
+    
+  });
+  await browser.close();
+
+  res.send({data});
+});
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`server iniciado na porta ${port} \n Acesse http://localhost:${port}`)
-})
-
-;(async () => {
-  
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(0);
-  await page.goto('https://einerd.com.br');
-  await page.screenshot({ path: 'print.png' });
-
-  await browser.close();
-})();
+  console.log(
+    `server iniciado na porta ${port} \nAcesse http://localhost:${port}`
+  );
+});
